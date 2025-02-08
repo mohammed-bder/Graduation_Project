98b3ab72edc6ad189be4431e04f2b18a2fc1c1d1
mohamdhazem <medozoma0@gmail.com>
2025-02-08 22:34:50 +0200

    PatientProfile&MetaData

---
+++ Graduation_Project.Api/DTO/Patient/PatientForProfileDto.cs
@@ -0,0 +1,33 @@
+﻿using Graduation_Project.Api.Attributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace Graduation_Project.Api.DTO.Patient
+{
+    public class PatientForProfileDto
+    {
+        [FullName]
+        [Required(ErrorMessage = "Full Name is required.")]
+        [StringLength(100, ErrorMessage = "Full Name must be at most 100 characters.")]
+        public string FullName { get; set; } // Fname + Lname
+
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email format.")]
+        public string Email { get; set; }
+
+        [Phone(ErrorMessage = "Invalid phone number format.")]
+        public string? PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "Gender is required.")]
+        [RegularExpression("^(Male|Female)$", ErrorMessage = "Gender must be either 'Male' or 'Female'.")]
+        public string Gender { get; set; } // Enum --> String (Readability)
+
+        [DataType(DataType.Date)]
+        [PastDate(ErrorMessage = "Date of Birth must be in the past.")]
+        public DateTime? DateOfBirth { get; set; }
+
+        [Url(ErrorMessage = "Invalid picture URL format.")]
+        public string? PictureUrl { get; set; }
+
+        public string? BloodTrpe { get; set; }
+    }
+}
---
+++ Graduation_Project.Core/Specifications/PatientSpecifications/PatientForProfileSpecs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graduation_Project.Core.Specifications.PatientSpecifications
+{
+    public class PatientForProfileSpecs : BaseSpecifications<Patient>
+    {
+        public PatientForProfileSpecs(string id):base(p => p.ApplicationUserId == id)
+        {
+            
+        }
+    }
+}
