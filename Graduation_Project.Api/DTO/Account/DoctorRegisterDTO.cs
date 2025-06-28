bee56d5ae30122ca42a1e4dbcb7ac1fcdac3465a
Mohammed Hassan Ahmed <123543218+MohammedHasanAhmed@users.noreply.github.com>
2025-06-29 00:07:39 +0300

    edit

--- Graduation_Project.Api/DTO/Account/DoctorRegisterDTO.cs
+++ Graduation_Project.Api/DTO/Account/DoctorRegisterDTO.cs
@@ -13,6 +13,8 @@ namespace Talabat.API.Dtos.Account
 
         [Required(ErrorMessage = "Email is required.")]
         [EmailAddress]
+        [RegularExpression("^(?!\\d)[a-zA-Z0-9._%+-]{3,}@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$",
+        ErrorMessage = "Email must not start with a number and must have at least 3 characters before '@'")]
         public string Email { get; set; }
 
         [Required(ErrorMessage = "Password is required.")]
--- Pharmacy_Dashboard.MVC/ViewModel/Account/RegisterViewModel.cs
+++ Pharmacy_Dashboard.MVC/ViewModel/Account/RegisterViewModel.cs
@@ -7,6 +7,8 @@ namespace Pharmacy_Dashboard.MVC.ViewModel.Account
 	{
 		[Required(ErrorMessage = "Email is required")]
 		[EmailAddress(ErrorMessage = "Invalid email address")]
+        [RegularExpression("^(?!\\d)[a-zA-Z0-9._%+-]{3,}@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$",
+		ErrorMessage = "Email must not start with a number and must have at least 3 characters before '@'")]
         public string Email { get; set; }
 
 		[Required(ErrorMessage = "Password is required.")]
