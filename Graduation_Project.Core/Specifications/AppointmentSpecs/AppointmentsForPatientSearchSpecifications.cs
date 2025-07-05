965f791f1638ef8cb76a9cc874fd96141db497a8
MoAlaa31 <mohamed.gad.eng@gmail.com>
2025-07-06 00:37:21 +0300

    fix logic

--- Graduation_Project.Core/Specifications/AppointmentSpecs/AppointmentsForPatientSearchSpecifications.cs
+++ Graduation_Project.Core/Specifications/AppointmentSpecs/AppointmentsForPatientSearchSpecifications.cs
@@ -4,16 +4,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Graduation_Project.Core.Specifications.AppointmentSpecs
 {
     public class AppointmentsForPatientSearchSpecifications: BaseSpecifications<Appointment>
     {
-        public AppointmentsForPatientSearchSpecifications(int patientId, DateOnly startDate)
+        public AppointmentsForPatientSearchSpecifications(int patientId, DateOnly date)
             : base(a =>
                 a.PatientId == patientId &&
-                a.AppointmentDate >= startDate &&
-                a.AppointmentDate < startDate.AddMonths(1) // Fetch appointments within 1 month
+                a.AppointmentDate == date
             )
         {
             Includes.Add(a => a.Doctor); // ✅ Includes Doctor
